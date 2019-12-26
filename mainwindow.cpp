d0b751f701067d5ede0a9b56bb8e984600fb3e74
Aufar Zakiev <zaufar@gmail.com>
2019-12-26 15:47:11 +0300

    Gitignored build fodler

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
-
